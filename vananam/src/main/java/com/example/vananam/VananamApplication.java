2dabe08f4a1a5432dd4cdc41aa341b353315f361
BDildar <133643663+BDildar@users.noreply.github.com>
2024-07-02 17:41:21 +0530

    Project Initial Setup

---
+++ vananam/src/main/java/com/example/vananam/VananamApplication.java
@@ -0,0 +1,13 @@
+package com.example.vananam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VananamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VananamApplication.class, args);
+	}
+
+}
---
+++ vananam/src/test/java/com/example/vananam/VananamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vananam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VananamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
